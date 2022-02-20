577ebf66b22aa78125b2813c7480a204a82d1987
taylor.holmes <taylorh2343@gmail.com>
2022-02-20 12:53:52 -0600

    added code from office hours

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,24 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  })
+  .then(data => res.json(data))
+  .catch(err => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+  .then(data => res.json(data))
+  .catch(err => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
@@ -19,6 +32,13 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => res.json(data))
+  .catch(err => res.status(500).json(err));
 });
 
 router.delete('/:id', (req, res) => {
